ecabaef6842b696168b23a69b6b889062265364f
idko-laptop <idko-laptop@fedora-hp.localdomain>
2022-06-20 17:17:48 -0400

    mamá ya no estoy probando github

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,5 +1,4 @@
 let canClick_login = true;
-console.log('hola mamá estoy probando github');
 document.getElementById('loginButton').addEventListener('click', async function(e)
 {
     if(theActualThing !== 'login') return;

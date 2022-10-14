9a128292043172b29a39b3334d35e6361d122f07
idko <ivancito72011@gmail.com>
2022-10-14 11:01:49 -0300

    mejorando navigationButtons

--- frontend/js/manageAccount/navigationButtons.js
+++ frontend/js/manageAccount/navigationButtons.js
@@ -7,33 +7,89 @@ function changeHashMenu(menuName)
 window.onhashchange = function()
 {
     const menu = hashEquals('menu');
-    if(menu === undefined) //Volver al index porque no hay menu
+    if(menu === undefined) // Volver al index porque no hay menu
     {
         alert('Go back to index');
         backToIndex();
     }
-    else if(menu !== actualMenu) //El menu no ha sido cambiado dentro de la pagina
+    else if(menu !== actualMenu) // El menu no ha sido cambiado dentro de la pagina
     {
-        //Moverse hacia ese menu si es que esta permitido.
+        // Moverse hacia ese menu si es que esta permitido.
         console.log('Menu en hash', menu);
 
         const bannedMenus =
         [
-            
+            'changeDataEmailCode',
+            'changeDataComprobingCode',
+            'deleteAccountEmailCode'
         ];
 
         const bannedLocalMenus =
         [
-            
+            'localCopy',
+            'changeData',
+            'changeDataEmailCode',
+            'changeDataComprobingCode',
+            'logOutInAll',
+            'deleteAccount',
+            'deleteAccountEmailCode'
         ];
 
-        if(!bannedMenus.includes(menu) && (!isLocalMode || !bannedLocalMenus.includes(menu))) //El menu es permitido
+        if(!bannedMenus.includes(menu) && (!isLocalMode || !bannedLocalMenus.includes(menu))) // El menu es permitido
         {
             const currentMenu = getElementByMenuName(actualMenu);
             const targetMenu = getElementByMenuName(menu);
+
+            if(targetMenu === undefined)
+            {
+                defaultMenu();
+                return;
+            }
+
+            specialCases(menu);
             
             actualMenu = menu;
             animatedTransition(currentMenu, targetMenu);
+        }
+        else defaultMenu();
+
+        function specialCases(menuName)
+        {
+            switch(menuName)
+            {
+                //TODO: poner los casos especiales, como spellcheck o el menú de actualizar datos
+            }
+        }
+
+        function defaultMenu()
+        {
+            console.log("Can't go there");
+            const currentMenu = getElementByMenuName(actualMenu);
+            const targetMenu = getElementByMenuName('main');
+            
+            if(currentMenu !== undefined) animatedTransition(currentMenu, targetMenu);
+            else
+            {
+                // Borrar todas las clases referentes a las animaciones
+                const openMenuClass = document.getElementsByClassName('openMenu');
+                for(let i = 0; i < openMenuClass.length; i++)
+                {
+                    openMenuClass[i].hidden = true;
+                    openMenuClass[i].classList.remove('openMenu');
+                }
+                const closeMenuClass = document.getElementsByClassName('closeMenu');
+                for(let i = 0; i < closeMenuClass.length; i++)
+                {
+                    closeMenuClass[i].classList.remove('closeMenu');
+                }
+
+                // Hacer visible el menú que queremos mostrar (main)
+                targetMenu.classList.add('openMenu');
+                targetMenu.hidden = false;
+            }
+
+            actualMenu = 'main';
+            changeHashMenu('main');
         }
     }
 }

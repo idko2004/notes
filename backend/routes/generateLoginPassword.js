1e803e2f649fde991904942f558f56f1cdffabbc
idko2004 <ivancito72011@gmail.com>
2023-02-11 16:24:13 -0300

    excluir ; porque sino las cookies no funcionan

--- backend/routes/generateLoginPassword.js
+++ backend/routes/generateLoginPassword.js
@@ -67,7 +67,7 @@ module.exports = function(app)
                     symbols: true,
                     lowercase: true,
                     uppercase: true,
-                    exclude: '"='
+                    exclude: '"=;'
                 });
 
 
